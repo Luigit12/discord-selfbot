5f0da78433ec675eb13600b5a5e6bcfab81500dd
luigit12 <58264257+Luigit12@users.noreply.github.com>
2022-03-19 19:36:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import discord
 import os
 from discord.ext import commands
-f = open("token.txt","r")
 
 client = commands.Bot(command_prefix="%", self_bot=True, help_command=None)
+f = open("token.txt","r")
 token = f.read()
 f.close()
+
+print(os.popen('echo 92#6163 | figlet | boxes -d unicornsay | lolcat').read())
 
 @client.event
 async def on_ready ():
